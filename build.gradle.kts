ecb1a589af1235c72d6801e49f2e52bf3a74a91e
sya-ri <sya79lua@gmail.com>
2021-12-02 15:39:15 +0900

    :rocket: Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "com.github.cheatank"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "CheaTank-common"
+
