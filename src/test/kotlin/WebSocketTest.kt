322de6fd5f8f373e38f8ce00f69282e475e42304
cheichey <cheichey.1223@gmail.com>
2021-12-08 09:37:54 +0900

    test: close() のテストを追加した

--- src/test/kotlin/WebSocketTest.kt
+++ src/test/kotlin/WebSocketTest.kt
@@ -4,11 +4,13 @@ import com.github.cheatank.common.PacketVersion
 import com.github.cheatank.common.data.EmptyPacketData
 import com.github.cheatank.server.application
 import io.ktor.application.Application
+import io.ktor.http.cio.websocket.Frame
 import io.ktor.server.testing.withTestApplication
 import utils.sendPacket
 import utils.testReceivePacket
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertIs
 
 class WebSocketTest {
     @Test
@@ -19,6 +21,16 @@ class WebSocketTest {
                 incoming.testReceivePacket(PacketType.SendVersion) {
                     assertEquals(it.int, PacketVersion)
                 }
+            }
+        }
+    }
+
+    @Test
+    fun closeTest() {
+        withTestApplication(Application::application) {
+            handleWebSocketConversation("/") { incoming, outgoing ->
+                outgoing.close()
+                assertIs<Frame.Close>(incoming.receive())
             }
         }
     }
